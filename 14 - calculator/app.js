8bdcbafa3b03e6686abcbee18c52d35a1d5f5047
galiphasandugan <galiphasandugan@gmail.com>
2023-05-27 17:58:31 +0300

    change

--- 14 - calculator/app.js
+++
@@ -1,5 +0,0 @@
-let groupliste= [
-    "ahmet","galip","durmuş","fikret","afra","fatma","musa","fatih a","fatih ç","begench","özgür","yasin","emre","celal","ilkay","alev","mehmet","oğuz","alper","serhat"
-]
-let randomSayı =Math.floor(Math.random()*20); 
-console.log(groupliste[randomSayı]);

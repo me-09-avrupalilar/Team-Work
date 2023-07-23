2a178357d904f3fd7ae07a497fd5e605d38c6283
galiphasandugan <galiphasandugan@gmail.com>
2023-07-23 21:12:38 +0200

    COMMİT

--- clarus_library/src/components/header/Header.jsx
+++ clarus_library/src/components/header/Header.jsx
@@ -1,8 +1,28 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
 
-const Header = () => {
+function ButtonDropdownsExample() {
   return (
-   <div></div>
-  )
+    <>
+      <InputGroup className="mb-3">
+        <Form.Control aria-label="Text input with dropdown button" />
+
+        <DropdownButton
+          variant="outline-secondary"
+          title="Dropdown"
+          id="input-group-dropdown-2"
+          align="end"
+        >
+          <Dropdown.Item href="#">ALL</Dropdown.Item>
+          <Dropdown.Item href="#">BOOKS</Dropdown.Item>
+          <Dropdown.Item href="#">MAGAZINES</Dropdown.Item>
+         
+        </DropdownButton>
+      </InputGroup>
+    </>
+  );
 }
 
-export default Header
+export default ButtonDropdownsExample;

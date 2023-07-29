6672730290d64b6ea266b5aabd22ae59a2648abb
conqueror1988 <fatih.aksoy1988@gmail.com>
2023-07-29 15:49:31 +0200

    star

--- 24-Film-info-start/src/components/FilmInfo.jsx
+++ 24-Film-info-start/src/components/FilmInfo.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
+import React from "react";
+import StarRating from "./StarRating";
 
-const FilmInfo = ({data}) => {
-  console.log(data)
+const FilmInfo = ({ data }) => {
+  console.log(data);
   return (
     <div>
-      {data.map((item)=>{
-        const {id,title,body,tags,date,rate}=item
-        return(
-            <div>
-              <h2>{title}</h2>
-              <p>{body}</p>
-              <p>{tags.join(", ")}</p>
-              <p className='rate text-danger fs-4'>{rate.toFixed(2)}{"/10"}</p>
-              
-
-            </div>
-        )
+      {data.map((item) => {
+        const { id, title, body, tags, date, rate } = item;
+        return (
+          <div>
+            <h2>{title}</h2>
+            <p>{body}</p>
+            <p>{tags.join(", ")}</p>
+            <p className="rate text-danger fs-4">
+              {rate.toFixed(2)}
+              {"/10"}
+            </p>
+            <StarRating rate={rate}/>
+          </div>
+          
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default FilmInfo
+export default FilmInfo;
--- 24-Film-info-start/src/components/StarRating.jsx
+++ 24-Film-info-start/src/components/StarRating.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Rating } from 'react-simple-star-rating'
 
-const StarRating = () => {
+const StarRating=({rate})=> {
+  console.log(rate)
+  const [rating, setRating] = useState(0)
+  const number=Math.floor(rate)
+console.log(number)
+  // Catch Rating value
+  const handleRating = (rate: number) => {
+    setRating(rate)
+
+    // other logic
+  }
+  // Optinal callback functions
+  // const onPointerEnter = () => console.log('Enter')
+  // const onPointerLeave = () => console.log('Leave')
+  // const onPointerMove = (value: number, index: number) => console.log(value, index)
+
   return (
-    <div>StarRating</div>
+    <div className='App'>
+      <Rating
+        onClick={handleRating}
+        // onPointerEnter={onPointerEnter}
+        // onPointerLeave={onPointerLeave}
+        // onPointerMove={onPointerMove}
+        /* Available Props */
+      />
+    </div>
   )
 }
-
 export default StarRating

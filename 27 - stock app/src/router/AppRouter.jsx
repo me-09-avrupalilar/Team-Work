eea8b4e705d6f133a8ae0c0eb20f81e87530fb43
fatih-celebi <celebifatih.c1@gmail.com>
2023-08-15 07:51:10 +0100

    add routes to dashboard items

--- 27 - stock app/src/router/AppRouter.jsx
+++ 27 - stock app/src/router/AppRouter.jsx
@@ -4,6 +4,11 @@ import Login from "../pages/Login";
 import Register from "../pages/Register";
 import PrivateRouter from "./PrivateRouter";
 import Dashboard from "../pages/Dashboard";
+import Purchases from "../pages/Purchases";
+import Firms from "../pages/Firms"
+import Brands from "../pages/Brands";
+import Products from "../pages/Products";
+import Sales from "../pages/Sales";
 
 const AppRouter = () => {
   return (
@@ -12,7 +17,14 @@ const AppRouter = () => {
         <Route path="/login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="stock" element={<PrivateRouter />}>
-          <Route path="" element={<Dashboard />} />
+          <Route path="" element={<Dashboard />}>
+            <Route path="purchases" element={<Purchases />}/>
+            <Route path="sales" element={<Sales />}/>
+            <Route path="firms" element={<Firms />}/>
+            <Route path="brands" element={<Brands />}/>
+            <Route path="products" element={<Products />}/>
+            
+          </Route>
         </Route>
       </Routes>
     </Router>
